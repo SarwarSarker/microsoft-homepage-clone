5d6962255f5ac7f65e8e08224daae501f9f6089f
Sarwar Sarker <54786929+SarwarSarker@users.noreply.github.com>
2020-10-27 16:21:56 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const show = document.querySelector('.show');
+const menubtn = document.querySelector('.menu-btn');
+const menubar = document.querySelector('.main-menu');
+
+menubtn.addEventListener('click', () => menubar.classList.toggle('show'))
+
